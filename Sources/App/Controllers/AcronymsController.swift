d7464d8f64b4e4956031438cf535b66524616dba
Diego <diegocaroli@icloud.com>
2018-04-15 19:53:12 +0200

    Split routes out into a separate controller

---
+++ Sources/App/Controllers/AcronymsController.swift
@@ -0,0 +1,82 @@
+import Vapor
+import Fluent
+
+struct AcronymsController: RouteCollection {
+  
+  func boot(router: Router) throws {
+    let acronymsRoutes = router.grouped("api", "acronyms")
+    
+    acronymsRoutes.post(use: createHandler)
+    acronymsRoutes.get(Acronym.parameter, use: getHandler)
+    acronymsRoutes.put(Acronym.parameter, use: updateHandler)
+    acronymsRoutes.delete(Acronym.parameter, use: deleteHandler)
+    acronymsRoutes.get(use: getAllHandler)
+    acronymsRoutes.get("search", use: searchHandler)
+    acronymsRoutes.get("first", use: getFirstHandler)
+    acronymsRoutes.get("sorted", use: sortedHandler)
+  }
+  
+  func createHandler(_ req: Request) throws -> Future<Acronym> {
+    return try req.content.decode(Acronym.self).flatMap(to: Acronym.self) { acronym  in
+      return acronym.save(on: req)
+    }
+  }
+  
+  func getHandler(_ req: Request) throws -> Future<Acronym> {
+    return try req.parameter(Acronym.self)
+  }
+  
+  func updateHandler(_ req: Request) throws -> Future<Acronym> {
+    return try flatMap(to: Acronym.self, req.parameter(Acronym.self), req.content.decode(Acronym.self)) { acronym, updatedAcronym in
+      acronym.short = updatedAcronym.short
+      acronym.long = updatedAcronym.long
+      
+      return acronym.save(on: req)
+    }
+  }
+  
+  func deleteHandler(_ req: Request) throws -> Future<HTTPStatus> {
+    return try req.parameter(Acronym.self).flatMap(to: HTTPStatus.self) { acronym in
+      return acronym.delete(on: req).transform(to: HTTPStatus.noContent)
+    }
+  }
+  
+  func getAllHandler(_ req: Request) throws -> Future<[Acronym]> {
+    return Acronym.query(on: req).all()
+  }
+
+  func searchHandler(_ req: Request) throws -> Future<[Acronym]> {
+    guard let searchTerm = req.query[String.self, at: "term"] else {
+      throw Abort(.badRequest)
+    }
+    return try Acronym.query(on: req).group(.or) { or in
+      try or.filter(\.short == searchTerm)
+      try or.filter(\.long == searchTerm)
+      }.all()
+  }
+  
+  func getFirstHandler(_ req: Request) throws -> Future<Acronym> {
+    return Acronym.query(on: req).first().map(to: Acronym.self) { acronym in
+      guard let acronym = acronym else {
+        throw Abort(.notFound)
+      }
+      return acronym
+    }
+  }
+  
+  func sortedHandler(_ req: Request) throws -> Future<[Acronym]> {
+    return try Acronym.query(on: req)
+      .sort(\.short, .ascending)
+      .all()
+  }
+  
+  //  router.get("api", "acronyms", "search") { req -> Future<[Acronym]> in
+  //    guard let searchTerm = req.query[String.self, at: "term"] else {
+  //      throw Abort(.badRequest)
+  //    }
+  //    return try Acronym.query(on: req)
+  //      .filter(\.short == searchTerm)
+  //      .all()
+  //  }
+
+}
